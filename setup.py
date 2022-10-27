f43118050eb08651125468bdc6542243f25bc6df
Dominik Krupke <d-krupke@users.noreply.github.com>
2022-10-27 18:46:29 +0200

    pep compliance

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
     name="flachtex",
-    version="0.3.8",
+    version="0.3.9",
     description="A traceable LaTeX flattener.",
     long_description=readme(),
     long_description_content_type="text/markdown",
